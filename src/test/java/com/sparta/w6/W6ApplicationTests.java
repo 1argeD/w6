acac5d9387ee89c31b4d0c2bfd8176198e78bde7
1argeD <94172354+1argeD@users.noreply.github.com>
2022-08-12 17:23:06 +0900

    Initial commit

---
+++ src/test/java/com/sparta/w6/W6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.w6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
